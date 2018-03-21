e793945488f3927cdfc2648b565176b9ea6d25cb
wiltrahan <twilorip@gmail.com>
2018-03-21 18:22:10 -0400

    extra number

---
+++ Arcade/atTheCrossroads/extraNumber.java
@@ -0,0 +1,9 @@
+// You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other. What is the value of the third integer?
+int extraNumber(int a, int b, int c) {
+    if(a == b) {
+        return c;
+    } else if(a == c) {
+        return b;
+    }
+    return a;
+}
